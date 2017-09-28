1c70220085fbca42982caecd80236513ee4b16dc
jiale.tan <jiale.tan@vungle.com>
2017-09-28 15:46:37 -0700

    fix(FooJsonDateTimeMessageParser.java): handle the datetime parsing failure

--- src/main/java/com/pinterest/secor/parser/FooJsonDateTimeMessageParser.java
+++ src/main/java/com/pinterest/secor/parser/FooJsonDateTimeMessageParser.java
@@ -1,12 +1,13 @@
 package com.pinterest.secor.parser;
 
+import com.google.common.net.HostAndPort;
 import com.pinterest.secor.common.SecorConfig;
 import com.pinterest.secor.message.Message;
+import com.timgroup.statsd.NonBlockingStatsDClient;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
-
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,30 +16,74 @@ import org.slf4j.LoggerFactory;
  * Created by foolim on 8/12/15.
  */
 public class FooJsonDateTimeMessageParser extends JsonMessageParser {
-    private static final Logger LOG = LoggerFactory.getLogger(FooJsonDateTimeMessageParser.class);
 
-    public static DateTimeFormatter badAdServerFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    private static final Logger LOG = LoggerFactory.getLogger(FooJsonDateTimeMessageParser.class);
+    private static DateTimeFormatter badAdServerFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    private static NonBlockingStatsDClient mStatsDClient;
 
     public FooJsonDateTimeMessageParser(SecorConfig config) {
         super(config);
+        if (mConfig.getStatsDHostPort() != null && !mConfig.getStatsDHostPort().isEmpty()) {
+            HostAndPort hostPort = HostAndPort.fromString(mConfig.getStatsDHostPort());
+            mStatsDClient = new NonBlockingStatsDClient(null, hostPort.getHostText(), hostPort.getPort());
+        }
     }
 
     @Override
     public long extractTimestampMillis(final Message message) {
-        JSONObject jsonObject = (JSONObject) JSONValue.parse(message.getPayload());
-        if (jsonObject != null) {
-            LOG.debug("FooJsonDateTimeMessageParser: jsonObject: " + jsonObject.toJSONString());
 
-            Object fieldValue = jsonObject.get(mConfig.getMessageTimestampName());
-            if (fieldValue != null) {
-                String timestampValue = fieldValue.toString();
-                if (timestampValue.contains("T")) {
-                    return DateTime.parse(timestampValue).toInstant().getMillis();
-                } else {
-                    return badAdServerFormat.parseDateTime(timestampValue).toInstant().getMillis();
+        // step 1: parse the message into json object
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(message.getPayload());
+        if (jsonObject == null) {
+            LOG.error("FooJsonDateTimeMessageParser: Invalid Json: " + message);
+            if (mStatsDClient != null) {
+                mStatsDClient.incrementCounter("secor.kafka." + message.getTopic().toLowerCase() + ".failed.json_parsing.count");
+            }
+
+            // send the bad messages back to 1970s
+            return 0;
+        }
+
+        LOG.debug("FooJsonDateTimeMessageParser: JsonObject: " + message);
+
+        // step 2: extract the value from timestamp field
+        Object fieldValue = jsonObject.get(mConfig.getMessageTimestampName());
+        if (fieldValue == null) {
+            LOG.error("FooJsonDateTimeMessageParser: Missing time field: " + jsonObject.toJSONString());
+            if (mStatsDClient != null) {
+                mStatsDClient.incrementCounter("secor.kafka." + message.getTopic().toLowerCase() + ".failed.date_missing.count");
+            }
+
+            // send the bad messages back to 1970s
+            return 0;
+        }
+
+        // step 3: try to stringify and parse the timestamp value
+        try {
+            String timestampValue = fieldValue.toString();
+            long ts;
+            if (timestampValue.contains("T")) {
+                ts = DateTime.parse(timestampValue).toInstant().getMillis();
+                if (mStatsDClient != null) {
+                    mStatsDClient.incrementCounter("secor.kafka." + message.getTopic().toLowerCase() + "succeeded.date_good.count");
+                }
+            } else {
+                ts = badAdServerFormat.parseDateTime(timestampValue).toInstant().getMillis();
+                if (mStatsDClient != null) {
+                    mStatsDClient.incrementCounter("secor.kafka." + message.getTopic().toLowerCase() + "succeeded.date_bad.count");
                 }
             }
+            return ts;
+        } catch (Throwable e) {
+            LOG.error("FooJsonDateTimeMessageParser: Invalid TimeStamp: " + message);
+            if (mStatsDClient != null) {
+                mStatsDClient.incrementCounter("secor.kafka." + message.getTopic().toLowerCase() + ".failed.date_parsing.count");
+            }
+
+            // send the bad messages back to 1970s
+            return 0;
         }
-        return 0;
+
+
     }
 }
